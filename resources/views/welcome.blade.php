9dce54b16bc1c7fcda408137b7f2c4af7365ae62
alhouranisimon-tech <alhouranisimon@gmail.com>
2026-05-03 23:45:47 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -98,7 +98,12 @@
     <div id="container" class="flex flex-col w-full h-full">
       <!-- Links Section -->
       <div id="links" class="grid grid-cols-4 max-md:grid-cols-2 w-full h-1/3 max-md:h-2/3">
-        
+
+        <!-- Link 4 -->
+        <a href="/" class="flex flex-col items-center justify-center bg-[url('/navimages/schedule.webp')] bg-cover bg-center relative" style="perspective: 1000px;">
+testing now
+        </a>
+          
         <!-- Link 1 -->
         <a href="/menu/Drinks" class="flex flex-col items-center justify-center text-white bg-[url('/activities/menu.webp')] bg-cover bg-center relative">
           <div class="absolute inset-0 bg-green-600 opacity-65"></div>
